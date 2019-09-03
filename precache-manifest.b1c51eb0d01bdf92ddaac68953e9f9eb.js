2c05e0c34dd46b738d71425c5f8fe9b8e9deb434
BlackMG27 <oike279@gmail.com>
2019-09-03 13:20:56 -0400

    Updates

---
+++ precache-manifest.b1c51eb0d01bdf92ddaac68953e9f9eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7004a6690a015f415ecba3ed37f41f5f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1f27b20a48635b8c4add",
+    "url": "/monster-rolodex/static/css/main.586ed73e.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monster-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "1f27b20a48635b8c4add",
+    "url": "/monster-rolodex/static/js/main.539927c4.chunk.js"
+  },
+  {
+    "revision": "b9d608cb1517ab0e40b5",
+    "url": "/monster-rolodex/static/js/runtime~main.8833da29.js"
+  }
+]);
